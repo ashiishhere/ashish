74de055d83ba30c631399086c2eab68c2d66c5d8
ashiishhere <ashiishhere@gmail.com>
2026-08-16 17:02:33 +0530

    Remove long/short form filters

--- app/(website)/portfolio/page.tsx
+++ app/(website)/portfolio/page.tsx
@@ -1,3 +1,5 @@
+export const revalidate = 0;
+
 import type { Metadata } from 'next';
 import { WorkHero } from '@/components/work/WorkHero';
 import { ProjectGrid } from '@/components/work/ProjectGrid';
